2e90d741442809a5ddd6099ef0b8793695d0192b
Redmi <terminator4444_4@mail.ru>
2026-08-11 01:07:59 +0500

    fixed server side crash

---
+++ src/main/java/org/betterx/betterend/client/EndParticleProviders.java
@@ -0,0 +1,43 @@
+package org.betterx.betterend.client;
+
+import org.betterx.betterend.particle.FireflyParticle;
+import org.betterx.betterend.particle.InfusionMistParticle;
+import org.betterx.betterend.particle.InfusionParticle;
+import org.betterx.betterend.particle.ParticleBlackSpore;
+import org.betterx.betterend.particle.ParticleGeyser;
+import org.betterx.betterend.particle.ParticleGlowingSphere;
+import org.betterx.betterend.particle.ParticleJungleSpore;
+import org.betterx.betterend.particle.ParticleSnowflake;
+import org.betterx.betterend.particle.ParticleSulphur;
+import org.betterx.betterend.particle.ParticleTenaneaPetal;
+import org.betterx.betterend.particle.PaticlePortalSphere;
+import org.betterx.betterend.particle.SmaragdantParticle;
+import org.betterx.betterend.registry.EndParticles;
+
+import net.fabricmc.fabric.api.client.particle.v1.ParticleProviderRegistry;
+
+final class EndParticleProviders {
+    private EndParticleProviders() {
+    }
+
+    static void register() {
+        ParticleProviderRegistry registry = ParticleProviderRegistry.getInstance();
+        registry.register(EndParticles.GLOWING_SPHERE, ParticleGlowingSphere.FactoryGlowingSphere::new);
+        registry.register(EndParticles.PORTAL_SPHERE, PaticlePortalSphere.FactoryPortalSphere::new);
+        registry.register(EndParticles.INFUSION, InfusionParticle.InfusionFactory::new);
+        registry.register(EndParticles.SULPHUR_PARTICLE, ParticleSulphur.FactorySulphur::new);
+        registry.register(EndParticles.GEYSER_PARTICLE, ParticleGeyser.FactoryGeyser::new);
+        registry.register(EndParticles.SNOWFLAKE, ParticleSnowflake.FactorySnowflake::new);
+        registry.register(EndParticles.AMBER_SPHERE, ParticleGlowingSphere.FactoryGlowingSphere::new);
+        registry.register(EndParticles.BLACK_SPORE, ParticleBlackSpore.FactoryBlackSpore::new);
+        registry.register(EndParticles.TENANEA_PETAL, ParticleTenaneaPetal.FactoryTenaneaPetal::new);
+        registry.register(EndParticles.JUNGLE_SPORE, ParticleJungleSpore.FactoryJungleSpore::new);
+        registry.register(EndParticles.FIREFLY, FireflyParticle.FireflyParticleFactory::new);
+        registry.register(EndParticles.SMARAGDANT, SmaragdantParticle.SmaragdantParticleFactory::new);
+        registry.register(EndParticles.INFUSION_MIST, InfusionMistParticle.FactoryInfusionMist::new);
+        registry.register(
+                EndParticles.INFUSION_MIST_NORTH,
+                sprites -> new InfusionMistParticle.FactoryInfusionMist(sprites, InfusionMistParticle.NORTH_RGB)
+        );
+    }
+}
--- src/main/java/org/betterx/betterend/registry/EndParticles.java
+++ src/main/java/org/betterx/betterend/registry/EndParticles.java
@@ -2,12 +2,10 @@ package org.betterx.betterend.registry;
 
 import org.betterx.bclib.particles.BCLParticleType;
 import org.betterx.betterend.BetterEnd;
-import org.betterx.betterend.particle.*;
+import org.betterx.betterend.particle.InfusionParticleType;
 
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
-
-import net.fabricmc.fabric.api.client.particle.v1.ParticleProviderRegistry;
 
 public class EndParticles {
     public static final SimpleParticleType GLOWING_SPHERE = register("glowing_sphere");
@@ -25,23 +23,6 @@ public class EndParticles {
     public static final SimpleParticleType SMARAGDANT = register("smaragdant_particle");
     public static final SimpleParticleType INFUSION_MIST = register("infusion_mist");
     public static final SimpleParticleType INFUSION_MIST_NORTH = register("infusion_mist_north");
-
-    public static void register() {
-        ParticleProviderRegistry.getInstance().register(GLOWING_SPHERE, ParticleGlowingSphere.FactoryGlowingSphere::new);
-        ParticleProviderRegistry.getInstance().register(PORTAL_SPHERE, PaticlePortalSphere.FactoryPortalSphere::new);
-        ParticleProviderRegistry.getInstance().register(INFUSION, InfusionParticle.InfusionFactory::new);
-        ParticleProviderRegistry.getInstance().register(SULPHUR_PARTICLE, ParticleSulphur.FactorySulphur::new);
-        ParticleProviderRegistry.getInstance().register(GEYSER_PARTICLE, ParticleGeyser.FactoryGeyser::new);
-        ParticleProviderRegistry.getInstance().register(SNOWFLAKE, ParticleSnowflake.FactorySnowflake::new);
-        ParticleProviderRegistry.getInstance().register(AMBER_SPHERE, ParticleGlowingSphere.FactoryGlowingSphere::new);
-        ParticleProviderRegistry.getInstance().register(BLACK_SPORE, ParticleBlackSpore.FactoryBlackSpore::new);
-        ParticleProviderRegistry.getInstance().register(TENANEA_PETAL, ParticleTenaneaPetal.FactoryTenaneaPetal::new);
-        ParticleProviderRegistry.getInstance().register(JUNGLE_SPORE, ParticleJungleSpore.FactoryJungleSpore::new);
-        ParticleProviderRegistry.getInstance().register(FIREFLY, FireflyParticle.FireflyParticleFactory::new);
-        ParticleProviderRegistry.getInstance().register(SMARAGDANT, SmaragdantParticle.SmaragdantParticleFactory::new);
-        ParticleProviderRegistry.getInstance().register(INFUSION_MIST, InfusionMistParticle.FactoryInfusionMist::new);
-        ParticleProviderRegistry.getInstance().register(INFUSION_MIST_NORTH, sprites -> new InfusionMistParticle.FactoryInfusionMist(sprites, InfusionMistParticle.NORTH_RGB));
-    }
 
     private static SimpleParticleType register(String name) {
         return BCLParticleType.register(BetterEnd.C.mk(name));
